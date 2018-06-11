4c239ff1ab2424a6c93184dabc5e5f3969f7dc0a
Justin McCarthy <justin@strongdm.com>
2018-06-11 10:55:49 -0700

    homebrew test

--- comply.rb
+++ comply.rb
@@ -15,4 +15,39 @@ class Comply < Formula
       bin.install "bin/comply"
     end
   end
+
+  test do
+    (testpath/"init_comply.sh").write <<~EOS
+      #!/usr/bin/expect -f
+      set timeout 2
+      spawn #{bin}/comply init
+      send -- "Hello Corporation\\r"
+      expect "*Filename Prefix*"
+      send -- "\\r\\r\\r"
+      expect "*GitHub*"
+      send -- "\\r"
+      expect "*Configure github now*"
+      send -- "\\r\\r"
+      expect "+:+"
+      send -- "thing\\r"
+      expect "+:+"
+      send -- "thing\\r"
+      expect "+:+"
+      send -- "thing\\r"
+      expect "+:+"
+      send -- "thing\\r"
+      expect "*Next steps*"
+      expect eof
+    EOS
+
+    chmod 0755, testpath/"init_comply.sh"
+    mkdir testpath/"rundir"
+
+    system "cd rundir && ../init_comply.sh"
+    assert_predicate testpath/"rundir/narratives", :exist?
+    assert_predicate testpath/"rundir/policies", :exist?
+    assert_predicate testpath/"rundir/procedures", :exist?
+    assert_predicate testpath/"rundir/standards", :exist?
+    assert_predicate testpath/"rundir/templates", :exist?
+  end
 end
