57e6458274db828bc7773f2951555a0a0871a4bb
Justin McCarthy <justin@strongdm.com>
2018-11-08 22:11:15 -0800

    updated to version v1.3.4

--- comply.rb
+++ comply.rb
@@ -1,8 +1,8 @@
 class Comply < Formula
   desc "Compliance automation framework, focused on SOC2"
   homepage "https://comply.strongdm.com"
-  url "https://github.com/strongdm/comply/archive/v1.3.3.tar.gz"
-  sha256 "1a4e4065db215768f78398190cf17b356f23cbf23392b659cd23780f8c44fe61"
+  url "https://github.com/strongdm/comply/archive/v1.3.4.tar.gz"
+  sha256 "b8834c29f6f38096a0175d9ded6af02cc16430fc2c59460bfbf1bfa5cff0f38a"
 
   depends_on "go" => :build
 
